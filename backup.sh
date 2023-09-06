193a30f6042dc65662a53e0f9d467ff8ae1b849a
Corletey <corleteya@gmail.com>
2023-09-06 13:15:01 +0000

    remove backup.sh

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Get the current date and time
-date=$(date +"%Y-%m-%d-%H-%M-%S")
-# Create a backup of the personal record file
-cp personal_records.txt "personal_records_$date.txt"
-# Print a message to the user
-echo "Backup created successfully."
